6ca7f79a8f5ffce35efb0d69d5385f5e728c6149
khan1104 <149855540+khan1104@users.noreply.github.com>
2025-05-09 17:10:46 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -3,13 +3,7 @@ import { getFirestore} from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvC4WvDZdozRo96r6PrYapfYw1wY1APmk",
-    authDomain: "tadkaai-11882.firebaseapp.com",
-    projectId: "tadkaai-11882",
-    storageBucket: "tadkaai-11882.firebasestorage.app",
-    messagingSenderId: "779583813454",
-    appId: "1:779583813454:web:634bec5210a8620989501f",
-    measurementId: "G-1CQ6X3749F"
+    //enter you credentials here
 };
 
 const app = initializeApp(firebaseConfig);
